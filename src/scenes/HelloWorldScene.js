4941449d0883bc07e2e44d90d99c1fc3628bcc07
Sunny de Graaf <sunnydegraaf@hotmail.nl>
2021-11-28 22:11:48 +0100

    Formatted code

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -16,34 +16,32 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     create() {
+        //init
         this.gameSpeed = 10;
         const { height, width } = this.game.config
+        this.health = 4;
+        this.lives = this.add.group();
+        this.cursorKeys = this.input.keyboard.createCursorKeys()
 
         //images
         this.add.image(0, 0, 'background').setOrigin(0, 0).setDepth(-1);
-
-        this.health = 3;
-
-        this.lives = this.add.group();
-
-        this.player = this.physics.add.image(400, 400, 'player').setDepth(1)
-        this.dumpster = this.physics.add.image(1000, 789, 'dumpster').setDepth(1)
-
         this.ground = this.add.tileSprite(0, height, width, 30, 'ground').setOrigin(0, 1).setDepth(0)
 
-        this.player.setCollideWorldBounds(true)
-        this.dumpster.setCollideWorldBounds(true)
-
-        this.cursorKeys = this.input.keyboard.createCursorKeys()
-
-        this.physics.add.collider(this.player, this.dumpster, this.renderLives, null, this);
-
+        //renders
         this.renderHealth();
+        this.renderPlayer();
+        this.renderDumpster();
+
+        //physics
+        this.physics.add.collider(this.player, this.dumpster, this.onCollision, null, this);
     }
 
     update() {
-        this.jump();
-        this.move();
+        if (this.player) {
+            this.jump();
+            this.move();
+        }
+
         this.ground.tilePositionX += this.gameSpeed
     }
 
@@ -53,13 +51,24 @@ export default class HelloWorldScene extends Phaser.Scene {
         }
     }
 
-    renderLives() {
+    renderPlayer() {
+        this.player = this.physics.add.image(400, 400, 'player').setDepth(1)
+        this.player.setCollideWorldBounds(true)
+    }
+
+    renderDumpster() {
+        this.dumpster = this.physics.add.image(1000, 789, 'dumpster').setImmovable().setDepth(1)
+        this.dumpster.setCollideWorldBounds(true)
+    }
+
+    onCollision() {
         this.live = this.lives.getFirstAlive();
         this.live.destroy();
+        this.player.setPosition(400, 400);
     }
 
     jump() {
-        if (this.cursorKeys.space.isDown) {
+        if (this.cursorKeys.space.isDown && this.player) {
             if (!this.player.body.onFloor()) {
                 return
             } else {
@@ -70,8 +79,8 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     move() {
-        if (this.cursorKeys.left.isDown) {
-            this.player.setVelocityX(-200)
+        if (this.cursorKeys.left.isDown && this.player) {
+            this.player.body.setVelocityX(-200)
         } else if (this.cursorKeys.right.isDown) {
             this.player.setVelocityX(200)
         } else {
